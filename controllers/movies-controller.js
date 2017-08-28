b8f8b963de18176989326557bcb1d3283864332a
Nital Patel <patelnital36@gmail.com>
2017-08-28 12:02:06 -0400

    Add movie model and render movie records in movies index page

--- controllers/movies-controller.js
+++ controllers/movies-controller.js
@@ -3,3 +3,20 @@ module.exports = {
     res.render('movies/index')
 }
 };
+
+const Movie = require('../models/movie');
+
+const moviesController = {};
+
+moviesController.index = (req, res) => {
+    Movie.findAll()
+        .then(movies => {
+        res.render('movies/index', { movies: movies });
+})
+.catch(err => {
+        res.status(400).json(err);
+});
+};
+
+module.exports = moviesController;
+
